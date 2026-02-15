b3a058ab8f9b516e635a1b8ad0326205b2a9d6f1
pyr <jzhangnv@gmail.com>
2026-02-15 12:07:36 -0800

    Added way to take a break

--- web/src/index.js
+++ web/src/index.js
@@ -1,3 +1,43 @@
-import "./styles.css";
+/**
+ * 1. Take in values of #workMin & #breakMin from template.html when #start is pressed
+ * 2. new Clock(workMin, breakMin, callback)
+ * 2a. callback should update #display
+ * 
+ */
 
-document.body.textContent = "deadbeef";
+import "./styles.css";
+import Clock from "./timer.js"
+
+let workMin = document.querySelector("#workMin").valueAsNumber;
+let breakMin = document.querySelector("#breakMin").valueAsNumber;
+const StartBtn = document.querySelector("#start");
+const StopBtn = document.querySelector("#stop");
+const BreakBtn = document.querySelector('#break');
+const Display = document.querySelector("#display");
+const Timer = new Clock(workMin, breakMin, updDisplay);
+
+StartBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  workMin = document.querySelector("#workMin").valueAsNumber;
+  breakMin = document.querySelector("#breakMin").valueAsNumber;
+  Timer.updateTime(workMin, breakMin);
+  Timer.switchWork();
+  Timer.start();
+});
+StopBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  Timer.stop();
+});
+BreakBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  Timer.switchBreak();
+  Timer.start();
+});
+function updDisplay(time){
+  Display.textContent = time / SECOND;
+  if (time == 0) {
+    BreakBtn.style.display = 'block';
+  } else {
+    BreakBtn.style.display = 'none';
+  }
+}
